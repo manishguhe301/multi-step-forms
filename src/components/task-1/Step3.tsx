0feb61fc889ae6a2a065ccaab7ad58e49f3404aa
Manish <manishguhe301@gmail.com>
2026-09-01 17:21:41 +0530

    feat(task-1): implement view selection UI in Step3
    Replaces the Step3 placeholder with a functional view selection component. This allows users to choose between 'List' and 'Board' layouts.
    
    - Added `StepHeader` for consistent step navigation
    - Implemented state management for view selection using `useState`
    - Integrated `lucide-react` icons (SquareText, SquareKanban)
    - Added responsive styling with Tailwind CSS and conditional border highlighting for the selected state

--- src/components/task-1/Step3.tsx
+++ src/components/task-1/Step3.tsx
@@ -1,6 +1,59 @@
+import { SquareKanban, SquareText } from "lucide-react"
+import StepHeader from "../elements/StepHeader"
+import { useState } from "react"
+
+const items = [
+  { title: "List", icon: SquareText },
+  { title: "Board", icon: SquareKanban },
+];
+
 const Step3 = () => {
+  const [selectedView, setSelectedView] = useState(items[0].title)
   return (
-    <div>Step3</div>
+    <div className="flex flex-col gap-4 py-4 ">
+      <StepHeader
+        stepTitle="Select a view"
+        subTitle="You can also customize this views in settings"
+      />
+
+      <div className="flex w-full flex-col gap-4 py-4 sm:flex-row sm:gap-6 ">
+        {items.map((item) => {
+          const isSelected = selectedView === item.title;
+          const Icon = item.icon;
+
+          return (
+            <div
+              key={item.title}
+              className="flex w-full flex-col items-center gap-2"
+            >
+              <button
+                type="button"
+                onClick={() => setSelectedView(item.title)}
+                className={`flex w-full cursor-pointer flex-col items-center justify-center gap-2 rounded-sm border-2 p-10 transition-colors ${isSelected
+                  ? "border-blue-500 shadow-sm hover:bg-blue-50"
+                  : "border-gray-300 hover:bg-gray-100"
+                  }`}
+              >
+                <Icon
+                  size={60}
+                  strokeWidth={1.5}
+                  className={isSelected ? "text-blue-500 transition-colors duration-200" : "text-gray-400"}
+                />
+              </button>
+
+              <span
+                className={`text-sm ${isSelected
+                  ? "font-semibold text-gray-600"
+                  : "font-medium text-gray-400"
+                  }`}
+              >
+                {item.title}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   )
 }
 
