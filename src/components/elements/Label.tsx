fc618903026f284e0d246cc99d5f4c4e0bcb34cb
Manish <manishguhe301@gmail.com>
2026-09-02 12:38:32 +0530

    feat: enhance Input and Label components with isTask2 prop for conditional styling

--- src/components/elements/Label.tsx
+++ src/components/elements/Label.tsx
@@ -1,14 +1,15 @@
 
-const Label = ({ htmlFor, label, desc }: {
+const Label = ({ htmlFor, label, desc, isTask2 = false }: {
   htmlFor?: string,
   label: string,
-  desc?: string
+  desc?: string,
+  isTask2?: boolean
 }) => {
   return (
     <div>
       <label
         htmlFor={htmlFor}
-        className="font-semibold text-base text-gray-700 capitalize"
+        className={` capitalize ${isTask2 ? 'text-[#5c6a84] font-normal text-sm' : 'text-gray-700 font-semibold text-base'}`}
       >
         {label}
       </label>
